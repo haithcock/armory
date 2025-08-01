695a5075f1a4594c2912de9090ecce717fb5dca4
haithcock <dakota.haith1997@.com>
2025-08-01 18:55:56 -0400

    STARTING OVER FOR CHRIST SAKE

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  role: { type: String, default: 'user' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('User', userSchema);
